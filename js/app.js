b1ed55e7cb3c948c49580d7ed2ec16e5cb74a191
Dima Kondratenko <kondratenkodima364@gmail.com>
2019-10-27 00:31:26 +0300

    icon like and kvadr

---
+++ js/app.js
@@ -0,0 +1,11 @@
+let productsCountEl =  document.getElementById("products-count");
+console.log(productsCountEl);
+
+let addToCartButtons = document.querySelectorAll("br + .moredetal");
+console.log(addToCartButtons);
+for(let i = 0; i < addToCartButtons.length; i++) {
+	addToCartButtons[i].addEventListener("click" , function() {
+      let prevProductsCount = +productsCountEl.textContent;
+      productsCountEl.textContent = prevProductsCount + 1;
+	})
+} 
