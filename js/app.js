67f38bd3d878b69b8f2ed7159dbc918b7c9c39c2
Dima Kondratenko <kondratenkodima364@gmail.com>
2019-10-27 20:29:49 +0200

    added slick slider liked

--- js/app.js
+++ js/app.js
@@ -18,5 +18,24 @@ for(let i = 0; i < addToCartButtons.length; i++) {
 
 let likeButtons = document.querySelectorAll(".heart");
 	console.log(likeButtons);
+for( let i = 0;   i < likeButtons.length; i++) {
+	likeButtons[i].addEventListener('click', function(){
+		 // if(likeButtons[i].classList.contains("liked")){
+		 // 	likeButtons[i].classList.remove("liked");
+		 // }else {
+		 // 	likeButtons[i].classList.add("liked");
+		 // }
+		 // likeButtons[i].classList.toggle("liked"); //Аналог if{} else{}
+		 //Еще есть обьектная модель но она нам в данной ситуации не подходит
+		 likeButtons[i].style.backgroundColor ="red";
+	})
+}
+
+ 
+$('.slider').slick({
+	dots: true,
+	autoplay:true,
+	autoplaySpeed: 2000,
 
 
+});
