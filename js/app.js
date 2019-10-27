219b69de27d16276ac6cee8d28b2cd9129e84d8a
Dima Kondratenko <kondratenkodima364@gmail.com>
2019-10-27 16:38:45 +0200

    added 8 cart-like

--- js/app.js
+++ js/app.js
@@ -7,5 +7,16 @@ for(let i = 0; i < addToCartButtons.length; i++) {
 	addToCartButtons[i].addEventListener("click" , function() {
       let prevProductsCount = +productsCountEl.textContent;
       productsCountEl.textContent = prevProductsCount + 1;
+      // Или одной строкой
+      // productsCountEl.textContent = +productsCountEl.textContent + 1;
+      // При решении вопроса удаляем консоли
 	})
 } 
+
+
+//change like state
+
+let likeButtons = document.querySelectorAll(".heart");
+	console.log(likeButtons);
+
+
