e7b46c8932d299e7bf1efdc194581a9b60dd5955
Dima Kondratenko <kondratenkodima364@gmail.com>
2019-12-31 13:50:04 +0200

    blabla

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
+let card = document.querySelectorAll('.price p');
+console.log(card);
+// card[0].classList.add('size');
+
+		  for(let i = 0; i < card.length; i++ ) {
+ 	          card[i].addEventListener('click' ,function(event){
+ 	          	card[i].style.color = 'red';
+ 	          });
+            };
+		// card[0].style.fontSize ='28px';
+		// card[0].style.color ='red';
+		 // card[0].classList.toggle("size")
+
+
 let productsCountEl =  document.getElementById("products-count");
 console.log(productsCountEl);
 
@@ -20,22 +34,22 @@ let likeButtons = document.querySelectorAll(".heart");
 	console.log(likeButtons);
 for( let i = 0;   i < likeButtons.length; i++) {
 	likeButtons[i].addEventListener('click', function(){
-		 // if(likeButtons[i].classList.contains("liked")){
-		 // 	likeButtons[i].classList.remove("liked");
-		 // }else {
-		 // 	likeButtons[i].classList.add("liked");
-		 // }
-		 // likeButtons[i].classList.toggle("liked"); //Аналог if{} else{}
+		  if(likeButtons[i].classList.contains("liked")){
+		 	likeButtons[i].classList.remove("liked");
+		 }else {
+		  	likeButtons[i].classList.add("liked");
+		  }
+		  // likeButtons[i].classList.toggle("liked"); //Аналог if{} else{}
 		 //Еще есть обьектная модель но она нам в данной ситуации не подходит
-		 likeButtons[i].style.backgroundColor ="red";
+		 // likeButtons[i].style.backgroundColor ="red";
 	})
 }
-
  
+
+
 $('.slider').slick({
 	dots: true,
 	autoplay:true,
 	autoplaySpeed: 2000,
-
 
 });
